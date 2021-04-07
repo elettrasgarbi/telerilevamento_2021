c89d3dbe39666d4fbf765fa151a7829d71f759a4
Elettra Sgarbi <81708629+elettrasgarbi@users.noreply.github.com>
2021-04-07 12:35:54 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -5,3 +5,22 @@
 #install.packages("raster")
 library(raster)
 setwd("C:/lab/greenland") # Windows
+
+# importiamo i dati tif uno a uno contenuti nella cartella greenland
+lst_2000 <- raster("lst_2000.tif")
+plot(lst_2000)
+
+lst_2005 <- raster ("lst_2005.tif")
+plot(lst_2005)
+
+lst_2010 <- raster ("lst_2010.tif")
+plot(lst_2010)
+
+lst_2015 <- raster ("lst_2015.tif")
+plot(lst_2015)
+
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
