76d7f92d923aee41eb7f7e4a7c7b81e36f3a71be
Elettra Sgarbi <81708629+elettrasgarbi@users.noreply.github.com>
2021-04-07 14:28:18 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -16,6 +16,9 @@ plot(lst_2005)
 lst_2010 <- raster ("lst_2010.tif")
 plot(lst_2010)
 
+#sovrapposizione delle 3 immagini 2000,2005 e 2010
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+
 lst_2015 <- raster ("lst_2015.tif")
 plot(lst_2015)
 
@@ -24,3 +27,18 @@ plot(lst_2000)
 plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
+
+#invece che importare i singoli dati, si usa la funzione stack e usarli tutti insieme 
+rlist <- list.files(pattern="tif")
+rlist
+import <- lapply(rlist,raster)
+TGr <- stack(import)
+plot(TGr)
+
+#plottiamo usando direttamente i dati uniti con uno stack
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
+plotRGB(TGr, 4, 3, 2, stretch="Lin")
+
+#installiano un'altra library
+library(rasterVis)
