a9a58788a8422b314d5444e3480ba12e12525e74
Elettra Sgarbi <81708629+elettrasgarbi@users.noreply.github.com>
2021-05-04 18:05:23 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,3 +3,7 @@ setwd("C:/lab/")
 
 library(knitr)
 
+#abbiamo preso il codice di greenland, copiati, incollati sul blocco note e salvati nella cartella lab nominando il file R_code_greenland.r
+
+stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#parte con la generazione del report e le firgure 
