cf0085cebbc32d12c705e66924ca0743c2945733
Elettra Sgarbi <81708629+elettrasgarbi@users.noreply.github.com>
2021-06-03 20:48:00 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,6 +1,7 @@
 # R_code_vegetation_indices.r
 # scarichiamo da virtuale le due immagine sullla cartella lab 
 library(raster) # require(raster) che fa la stessa cosa di library
+library(RStoolbox) # for vegetation indices calculation
 
 setwd("C:/lab/") # Windows
 
@@ -27,7 +28,7 @@ plot(dvi1)
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 plot(dvi1, col=cl, main="DVI at time 1")
 
- #ora facciamo la stessa cosa per il defor2
+#ora facciamo la stessa cosa per il defor2
 defor2
 #names      : defor2.1, defor2.2, defor2.3 
 dvi2 <- defor2$defor2.1 - defor2$defor2.2
@@ -37,6 +38,52 @@ plot(dvi2, col=cl, main="DVI at time 2")
 par(mfrow=c(2,1))
 plot(dvi1, col=cl, main="DVI at time 1")
 plot(dvi2, col=cl, main="DVI at time 2")
+
+# ora facciamo la differenza delle due mappe 
+difdvi <- dvi1 - dvi2 
+# mi dice che l'estensione delle due immagini non è la stessa, molto probabilmente una delle due immagini ha qualche pixer in più 
+# ma non è un problema perchè calcolerà il dato nella loro intersezione 
+
+# dev.off()
+# andiamo ad evidenziare la differenza con colour and palette e abbiamo la mappa su una sola banda 
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdvi, col=cld)
+# dove la differenza è più forte avremo un colore rosso, viceversa un colore blu/bianco 
+# un immagine a 16 b ha dei valori che vanno da 0 a 2^16= 65536
+#quindi nel primo tempo ho 255 nella seconda 65535 e non ha senso confrontarle allora usiamo un altro indice
+# NDVI = normalizzazione della somma, con questo comando possiamo comparare immagini che hanno valori diversi di entrata 
+# il ranhe del NDVI ha sempre lo stesso range cioè minumo di -1 e massimo di +1
+#si calcola 
+# (NIR-RED) / (NIR+RED)
+ndvi1 <- (defor1$defor1.1 - defor1$defor1.2) / (defor1$defor1.1 + defor1$defor1.2)
+plot (ndvi1, col=cl)
+
+# ndvi1 <- dvi1 / (defor1$defor1.1 + defor1$defor1.2)
+# plot (ndvi1, col=cl)
+
+# nel pacchetto RStoolbox si trova la funzione spectralIndices che possiamo utilizzare per fare il calcolo 
+
+# andiamo a calcoare i valori della seconda immagine e per vedere lancio il comando defor2 
+
+ndvi2 <- (defor2$defor2.1 - defor2$defor2.2) / (defor2$defor2.1 + defor2$defor2.2)
+plot (ndvi2, col=cl)
+
+# RStoolbox::spectralIndices
+vi1 <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
+plot(vi2, col=cl)
+
+# stessa cosa anche per la seconda 
+vi2 <- spectralIndices(defor2, green = 3, red = 2, nir = 1)
+plot(vi2, col=cl)
+
+# fare la differenza tra i due ndvi
+difndvi <- ndvi1 - ndvi2
+
+ 
+
+# dev.off()
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difndvi, col=cld)
 
 
 
